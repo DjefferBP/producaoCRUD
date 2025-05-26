ae79b1b0c7e01f614253e9662d00bab891c5b7d3
DjefferBP <djefferbervianprange123@gmail.com>
2025-05-26 17:19:01 -0300

    Add files via upload

---
+++ projeto industria/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/ProdGraph.ico">
+    <title>Dashboard da produção</title>
+    <?php echo "<link rel='stylesheet' href='styles/index.css'>" ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <!-- Cabeçalho do site -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                    <hr>
+                    </div>
+                </div>
+            </div>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <a href="index.php">
+                        <img src="img/logo.svg" alt="Logo da empresa" class="lixo" width="300px" height="auto" style="float: right; margin-right: 50px; margin-top: 15px; width: 15vw; height: auto;">
+                    </a>
+                    <div class="usuarioDiv">
+                        <h4>Bem vindo!</h4>
+                    </div>
+                </div>
+            </nav>
+                    
+        </nav>
+    </header>
+</body>
+
+</html>

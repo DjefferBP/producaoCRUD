8b05684bb1aab92f4e613fbbad692842039043bb
DjefferBP <djefferbervianprange123@gmail.com>
2025-06-04 17:06:14 -0300

    Update tolerancia.php

--- dadosProducao/tolerancia.php
+++ dadosProducao/tolerancia.php
@@ -1,12 +1,33 @@
 <?php 
-    if(!isset($_POST['editarTolerancia'])){
+    if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+    }
+
+    if (!file_exists("tolerancia.json")) {
         $quantidade = $_SESSION['quantidades'];
         $somaQt = array_sum($quantidade);
-        $tolerancia = $somaQt * 0.02;
-        echo "<h4 style='font-size: 22px'><b>2%</b>(".round($tolerancia).")</h4>";
-    } else {
-        $tolerancia = $_POST['editarTolerancia'];
-        $somaQt = array_sum($quantidade);
+
+        $tolerancia = $_SESSION['tole'][0] ?? 2;
         $tolerar = $somaQt * ($tolerancia / 100);
-        echo "<h4 style='font-size: 22px'><b>$tolerancia%</b>(".round($tolerar).")</h4>";
+
+        echo "<h4 style='font-size: 22px'><b>{$tolerancia}%</b>(".round($tolerar).")</h4>";
+    } else {
+        $quantidade = $_SESSION['quantidades'];
+        $somaQt = array_sum($quantidade);
+        $toleranciaArray = json_decode(file_get_contents("tolerancia.json"), true);
+        $tolerancia = $toleranciaArray[0];
+        $tolerar = $somaQt * ($tolerancia / 100);
+
+        echo "<h4 style='font-size: 22px'><b>{$tolerancia}%</b>(".round($tolerar).")</h4>";
+    }
+
+
+    if(isset($_POST['editarTolerancia'])){
+        $tolerancia = $_POST['editarTolerancia'];
+        $_SESSION['tole'][0] = $tolerancia;
+        $tolerar = $somaQt * ($tolerancia / 100);
+        
+        file_put_contents("tolerancia.json", json_encode($_SESSION['tole'], JSON_PRETTY_PRINT));
+        header("Location: ../inicial.php");
+        exit;
     }
